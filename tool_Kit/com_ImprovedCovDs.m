5cce5a5483aaec38c5dc1c6d75ad71df13390e07
Kai-Xuan Chen <36260120+Kai-Xuan@users.noreply.github.com>
2019-02-08 17:30:19 +0800

    Update com_ImprovedCovDs.m

--- tool_Kit/com_ImprovedCovDs.m
+++ tool_Kit/com_ImprovedCovDs.m
@@ -1,5 +1,5 @@
 %%  Compute CSPD matrices
-%   CSPD: A low-dimensional discriminative data descriptor than traditional covariance descriptors for image set classification
+%   CSPD: a special type of improved CovDs(iCovDs) 
 %   Written by Kai-Xuan Chen (e-mail: kaixuan_chen_jsh@163.com)
 %   version 2.0 -- December/2018 
 %   version 1.0 -- June/2017 
@@ -32,31 +32,58 @@ function final_ImprovedCovDs = com_ImprovedCovDs(block_Matrix,option)
             for blo_th = 1:num_Block
                 temp_Block = block_Matrix(:,:,blo_th);
                 temp_Cov = cov(temp_Block');
-                if norm(temp_Cov,'fro') == 0 && trace(temp_Cov) == 0
+                [~,S] = eig(temp_Cov);
+                [temp_min,~] = min(diag(S));
+                if temp_min <= 10^(-9)
+                    temp_Block = temp_Block + rand(size(temp_Block));
+                    temp_Cov = cov(temp_Block');
+                    [~,S] = eig(temp_Cov);
+                    [temp_min,~] = min(diag(S));
+                    while temp_min <= 10^(-9)
+                        temp_Cov = temp_Cov + 0.001*trace(temp_Cov)*eye(dim);
+                        [~,S] = eig(temp_Cov);
+                        [temp_min,~] = min(diag(S));
+                    end  
+                elseif norm(temp_Cov,'fro') == 0 && trace(temp_Cov) == 0
                     temp_Cov = eye(dim);
-                else
-                    temp_Cov = temp_Cov + 0.001*trace(temp_Cov)*eye(dim);
-                end
+                end 
                 logm_TempCov = logm(temp_Cov);
                 logm_Block_Representations(:,:,blo_th) = logm_TempCov;
             end
             vec_Block_Representations = reshape(logm_Block_Representations,dim*dim,num_Block);
-%         case 'Gauss'
-%             logm_Block_Representations = zeros(dim+1,dim+1,num_Block);
-%             for blo_th = 1:num_Block
-%                 temp_Block = block_Matrix(:,:,blo_th);
-%                 temp_Cov = embed_Gauss2SPD(temp_Block,option);
-%                 if norm(temp_Cov,'fro') == 0 && trace(temp_Cov) == 0
-%                     temp_Cov = eye(dim);
-%                 else
-%                     temp_Cov = temp_Cov + 0.001*trace(temp_Cov)*eye(dim+1);
-%                 end
-%                 logm_TempCov = logm(temp_Cov);
-%                 logm_Block_Representations(:,:,blo_th) = logm_TempCov;
-%             end     
-%             vec_Block_Representations = reshape(logm_Block_Representations,(dim+1)*(dim+1),num_Block);
+        case 'Gauss'
+            logm_Block_Representations = zeros(dim+1,dim+1,num_Block);
+            for blo_th = 1:num_Block
+                temp_Block = block_Matrix(:,:,blo_th);
+                temp_Cov = embed_Gauss2SPD(temp_Block,option);
+                [~,S] = eig(temp_Cov);
+                [temp_min,~] = min(diag(S));
+                if temp_min <= 10^(-9)
+                    temp_Block = temp_Block + rand(size(temp_Block));
+                    temp_Cov = embed_Gauss2SPD(temp_Block,option);
+                    [~,S] = eig(temp_Cov);
+                    [temp_min,~] = min(diag(S));
+                    while temp_min <= 10^(-9)
+                        temp_Cov = temp_Cov + 0.001*trace(temp_Cov)*eye(dim+1);
+                        [~,S] = eig(temp_Cov);
+                        [temp_min,~] = min(diag(S));
+                    end                    
+                elseif norm(temp_Cov,'fro') == 0 && trace(temp_Cov) == 0
+                    temp_Cov = eye(dim+1);
+                end 
+                logm_TempCov = logm(temp_Cov);
+                logm_Block_Representations(:,:,blo_th) = logm_TempCov;
+            end     
+            vec_Block_Representations = reshape(logm_Block_Representations,(dim+1)*(dim+1),num_Block);
     end
-    
     ImprovedCovDs = vec_Block_Representations'*vec_Block_Representations;
-    final_ImprovedCovDs = ImprovedCovDs + 0.001*trace(ImprovedCovDs)*eye(num_Block);
+    [~,S] = eig(ImprovedCovDs);
+    [temp_min,~] = min(diag(S));
+    while temp_min <= 10^(-9)
+        ImprovedCovDs = ImprovedCovDs + 0.001*trace(ImprovedCovDs)*eye(num_Block);
+        [~,S] = eig(ImprovedCovDs);
+        [temp_min,~] = min(diag(S));
+    end 
+    
+    final_ImprovedCovDs = ImprovedCovDs;
 end
